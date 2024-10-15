0f123edec4e39ba29b98ae6888419b86f4a93277
Mreoch1 <Mreoch82@hotmail.com>
2024-10-14 23:03:10 -0400

    Update for Heroku deployment with Node.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
 
-// Load environment variables
-dotenv.config();
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env,
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
-  server: {
-    host: true
-  }
 });
